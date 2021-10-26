de2a8e4695645128e8c66c74683b4d4f8c3b3a72
eamon <eammiller@outlook.com>
2021-10-26 15:53:03 -0400

    initial files
    upload of initial website

--- src/components/ProductSection.js
+++ src/components/ProductSection.js
@@ -4,31 +4,11 @@ import Prodcard from "./Productcard";
 function ProdSection(props) {
   return (
     <PSection>
-      <Prodcard
-
-        title="big tee shirt"
-        price="$5555"
-      />
-      <Prodcard
-
-        title="pajamas"
-        price="$566"
-      />
-      <Prodcard
-
-        title="crop top"
-        price="$3"
-      />
-      <Prodcard
-
-        title="track pants"
-        price="$22"
-      />
-      <Prodcard
-
-        title="hoodie"
-        price="$9"
-      />
+      <Prodcard title="big tee shirt" price="$5555" />
+      <Prodcard title="pajamas" price="$566" />
+      <Prodcard title="crop top" price="$3" />
+      <Prodcard title="track pants" price="$22" />
+      <Prodcard title="hoodie" price="$9" />
     </PSection>
   );
 }
@@ -36,7 +16,7 @@ function ProdSection(props) {
 export default ProdSection;
 
 const PSection = styled.div`
-    justify-content: space-evenly;
+  justify-content: space-evenly;
   width: 100%;
   display: flex;
   flex-wrap: wrap;
